b683de91917fc538bfa5d90be2d87bb1f3892a5a
rmodrego <rmodrego@indra.es>
2021-11-05 09:22:08 +0100

    initial commit

---
+++ src/test/java/com/modrego/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modrego.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
